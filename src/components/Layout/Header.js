8408cd96748c68376b06b5c6c57f39d49176e18d
Nazar <nazar.varnava@gmail.com>
2022-10-24 15:01:09 +0300

    Add cart button and some initial logic

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,19 +1,21 @@
-import React from "react";
+import React, { Fragment } from "react";
+
+import HeaderCartButton from "./HeaderCartButton";
 
 import mealsImage from "../../assets/meals.jpg";
 import classes from "./Header.module.css";
 
 const Header = (props) => {
   return (
-    <React.Fragment>
+    <Fragment>
       <header className={classes.header}>
         <h1>ReactMeals</h1>
-        <button>Cart</button>
+        <HeaderCartButton />
       </header>
       <div className={classes["main-image"]}>
         <img src={mealsImage} alt="Img" />
       </div>
-    </React.Fragment>
+    </Fragment>
   );
 };
 
